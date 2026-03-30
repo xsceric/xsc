285b794f60d2a65bf2b9c4841870dbdb77e1a0ce
xsceric <141149726+xsceric@users.noreply.github.com>
2026-03-30 12:22:28 +0000

    Deploying to gh-pages from @ xsceric/xsc@06b2c6046a48c35e58a28a37951d1527ae51d534 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,21 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/xsc/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/xsc/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/xsc/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
