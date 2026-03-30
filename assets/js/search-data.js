98ec26ca5268bf5258133c13f92f9dc18f22f5e2
xsceric <141149726+xsceric@users.noreply.github.com>
2026-03-30 13:31:00 +0000

    Deploying to gh-pages from @ xsceric/xsc@14b54e325d9266bdabd0114379aa229668656b74 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,12 +45,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/xsc/books/the_godfather/";
-            },},{
+      },{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
